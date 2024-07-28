5450992cbc2ab49f7634783e4696dbe8cd52cb92
Gaurav Singh <gauravnumber@gmail.com>
2024-07-28 12:36:42 +0530

    Description added

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Texts",
-  description: "Share your wisdom anonymously.",
+  title: "Manuscripts",
+  description: "Unlock the power of anonymous sharing with our platform. Share valuable life lessons, expert advice, or personal insights without revealing your identity. Contribute to a community eager for guidance while maintaining your privacy. Make a meaningful impact with your knowledge today!",
 };
 
 export default function RootLayout({ children }) {
