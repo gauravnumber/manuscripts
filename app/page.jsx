0d67a7ab40841657c9f0c8e17edc0af005093c4d
Gaurav Singh <gauravnumber@gmail.com>
2024-07-29 19:04:41 +0530

    subtle

--- app/page.jsx
+++ app/page.jsx
@@ -45,7 +45,7 @@ export default function Home() {
     <main className="w-full flex flex-col p-2">
       <header className="head_text  text-center">Manuscripts</header>
       <p className="mt-5 px-3 text-gray-400  text-center sm:text-xl">
-        Share your hidden wisdom; it may hold the greatest value. Expressing
+        Share your hidden wisdom; It may hold the greatest value. Expressing
         these thoughts not only lifts the burden of secrecy but also enriches
         others with your unique perspective. Your insights could inspire, guide,
         or transform someone’s life. Don’t let fear or doubt keep your wisdom
