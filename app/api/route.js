437aa7714283c88d25334115410a98b669110b12
Gaurav Singh <gauravnumber@gmail.com>
2024-07-31 12:33:56 +0530

    /api cache

--- app/api/route.js
+++ app/api/route.js
@@ -4,7 +4,11 @@ export const GET = async (response) => {
     try {
         const manuscripts = await collection.find().sort({ createdAt: -1 }).toArray()
         // console.log(manuscripts)
-        return new Response(JSON.stringify(manuscripts), { status: 200 })
+        return new Response(JSON.stringify(manuscripts), {
+            status: 200, headers: {
+                'Cache-Control': 'no-store'
+            }
+        })
 
     } catch (error) {
         return new Response("Failed to fetch manuscripts", { status: 500 })
