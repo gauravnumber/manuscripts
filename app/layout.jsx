8dc43c888ebc3eff394cc1307314e51ec9c48bd9
Gaurav Singh <gauravnumber@gmail.com>
2024-08-20 12:34:03 +0530

    server actionn completed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,17 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Manuscripts",
   description: "Unlock the power of anonymous sharing with our platform. Share valuable life lessons, expert advice, or personal insights without revealing your identity. Contribute to a community eager for guidance while maintaining your privacy. Make a meaningful impact with your knowledge today!",
 };
 
-export const dynamic = 'force-dynamic'
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
