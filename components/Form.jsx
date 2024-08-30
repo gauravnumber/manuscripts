b0ad4c7c5a624f90f8d72655599fbebec8dee861
Gaurav Singh <gauravnumber@gmail.com>
2024-08-30 20:30:34 +0530

    add components

---
+++ components/Form.jsx
@@ -0,0 +1,30 @@
+"use client"
+import { addManuscript } from "@lib/actions";
+
+const Form = () => {
+  const formAction = async (formData) => {
+    const text = formData.get("manuscript")
+    await addManuscript(text)
+  }
+
+  return (
+    <form
+      className="w-full p-3 flex flex-col gap-2"
+      action={formAction}
+    >
+      <textarea
+        className="text-slate-300  h-40 p-4 border bg-slate-800 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none"
+        placeholder="Type your text here..."
+        autoFocus
+        name="manuscript"
+      />
+      <input
+        type="submit"
+        value="Submit"
+        className="bg-slate-500 p-2 rounded-lg w-20 "
+      />
+    </form>
+  );
+};
+
+export default Form;
---
+++ components/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <>
+      <header className="head_text  text-center">Manuscripts</header>
+      <p className="mt-5 px-3 text-gray-400  text-center sm:text-xl">
+        Share your hidden wisdom; It may hold the greatest value. Expressing
+        these thoughts not only lifts the burden of secrecy but also enriches
+        others with your unique perspective. Your insights could inspire, guide,
+        or transform someone&apos;s life. Don&apos;t let fear or doubt keep your
+        wisdom concealed; embrace the chance to contribute.
+      </p>
+    </>
+  );
+};
+
+export default Hero;
