19b7056a34ba483e6037eb581bdc2c87410a765a
Gaurav Singh <gauravnumber@gmail.com>
2024-09-02 16:35:49 +0530

    unneccessary removed

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,6 @@ import Form from "@components/Form";
 import Hero from "@components/Hero";
 import Manuscripts from "@components/Manuscripts";
 
-// export const experimental_ppr = true;
-// export const dynamic = "force-dynamic";
-
 export default async function Home() {
   return (
     <main className="w-full flex flex-col p-2">
